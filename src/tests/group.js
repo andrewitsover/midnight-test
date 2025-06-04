b0b78d0da54c3f5d18d138e64a2b38b72a1546e5
Andrew Jones <thebinarysearchtree@gmail.com>
2025-06-04 18:58:34 +1000

    groupBy includes

--- src/tests/group.js
+++ src/tests/group.js
@@ -1,7 +1,7 @@
 import { strict as assert } from 'assert';
 import { test } from '../run.js';
 
-test('group', async (context) => {
+test('groupBy', async (context) => {
   const db = context.common.db;
   const towns = await db.fighters
     .groupBy('hometown')
@@ -14,4 +14,28 @@ test('group', async (context) => {
       }
     });
   assert.equal(towns.at(1).height, 173);
+  const events = await db.events
+    .groupBy('locationId')
+    .count({
+      include: {
+        location: (t, c) => t.locations.get({ id: c.locationId })
+      },
+      limit: 3
+    });
+  const event = events.at(1);
+  assert.equal(event.locationId, event.location.id);
+  assert.equal(event.count, 2);
+  const locations = await db.locations.query({
+    include: {
+      events: (t, c) => t.events
+        .groupBy('locationId')
+        .count({
+          where: {
+            locationId: c.id
+          }
+        })
+    },
+    limit: 3
+  });
+  console.log(locations.at(0).events);
 });
