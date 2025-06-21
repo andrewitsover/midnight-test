a89e7db336cc33d6c60e3b3f585943f6d2192baf
Andrew Jones <thebinarysearchtree@gmail.com>
2025-06-21 20:01:21 +1000

    bug

--- src/results/sqlite.d.ts
+++ src/results/sqlite.d.ts
@@ -1357,8 +1357,8 @@ interface TypedDb {
   cancelledFights: Queries<CancelledFight, InsertCancelledFight, ToWhere<CancelledFight & unknown>, unknown, number, TypedDb>;
   titleRemovals: Queries<TitleRemoval, InsertTitleRemoval, ToWhere<TitleRemoval & unknown>, unknown, number, TypedDb>;
   fighterProfiles: VirtualQueries<FighterProfile, ToWhere<FighterProfile & unknown>>;
-  opponents: Omit<Queries<Opponent, InsertOpponent, ToWhere<Opponent & unknown>, unknown, undefined, TypedDb>, 'remove' | 'insert' | 'insertMany' | 'update' | 'upsert'>;
-  detailedFighters: Omit<Queries<DetailedFighter, InsertDetailedFighter, ToWhere<DetailedFighter & unknown>, unknown, undefined, TypedDb>, 'remove' | 'insert' | 'insertMany' | 'update' | 'upsert'>;
+  opponents: Omit<Queries<Opponent, undefined, ToWhere<Opponent & unknown>, unknown, undefined, TypedDb>, 'remove' | 'insert' | 'insertMany' | 'update' | 'upsert'>;
+  detailedFighters: Omit<Queries<DetailedFighter, undefined, ToWhere<DetailedFighter & unknown>, unknown, undefined, TypedDb>, 'remove' | 'insert' | 'insertMany' | 'update' | 'upsert'>;
   exec(sql: string): Promise<void>;
   begin(): Promise<void>;
   commit(): Promise<void>;
