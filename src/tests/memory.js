5977bfef9aedda381f2e931950510ea929b21804
Andrew Jones <thebinarysearchtree@gmail.com>
2026-01-03 06:29:05 +1000

    add in-memory support

---
+++ src/tests/memory.js
@@ -0,0 +1,20 @@
+import { SQLiteDatabase, Table } from 'flyweightjs';
+import { test } from '../run.js';
+import { strict as assert } from 'assert';
+
+class Users extends Table {
+  id = this.IntPrimary;
+  name;
+}
+
+test('memory', async () => {
+  const database = new SQLiteDatabase();
+  const db = database.getClient({ Users });
+  const sql = db.diff();
+  await db.migrate(sql);
+  await db.users.insert({ name: 'Andrew' });
+  const users = await db.users.many();
+  assert.equal(users.length, 1);
+  assert.equal(users.at(0).name, 'Andrew');
+  await database.close();
+});
