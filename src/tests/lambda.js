2d200806c770513bc07039bebea463da173f9634
Andrew Jones <thebinarysearchtree@gmail.com>
2026-05-19 17:57:28 +1000

    user-defined functions

--- src/tests/lambda.js
+++ src/tests/lambda.js
@@ -3,11 +3,29 @@ import { test } from '../run.js';
 import { strict as assert } from 'assert';
 import { randomUUID } from 'crypto';
 
-class Users extends BaseTable {
-  id = this.Function(this.TextPrimary, () => randomUUID());
-  name;
-}
 const database = new Database(':memory:');
+const uuid = database.createFunction({
+  returnType: Table.TextPrimary,
+  lambda: () => randomUUID()
+});
+const compare = database.createFunction({
+  returnType: Table.Int,
+  options: {
+    deterministic: true
+  },
+  lambda: (a, b) => {
+    const { from, compare } = Temporal.ZonedDateTime;
+    const d = from(a);
+    const e = from(b);
+    return compare(d, e);
+  }
+});
+
+class Users extends BaseTable {
+  id = uuid();
+  name;
+  createdAt = this.Null.ZonedDateTime;
+}
 const db = database.getClient({ Users });
 const sql = db.diff();
 db.migrate(sql);
@@ -44,4 +62,36 @@ test('upsert lambda', async () => {
   });
   const user = db.users.get({ id });
   assert.equal(user !== undefined, true);
+});
+
+test('function in where', async () => {
+  db.users.delete();
+  const now = Temporal.Now.zonedDateTimeISO();
+  const rows = [
+    {
+      name: 'Andrew',
+      createdAt: now.subtract({ days: 1 })
+    },
+    {
+      name: 'Penelope',
+      createdAt: now.add({ days: 1 })
+    },
+    {
+      name: 'James',
+      createdAt: now.add({ days: 2 })
+    }
+  ];
+  db.users.insertMany(rows);
+  const users = db.query(c => {
+    const { users: u, gte } = c;
+    const compared = compare(u.createdAt, now);
+    return {
+      select: u,
+      where: {
+        [compared]: gte(0)
+      }
+    }
+  });
+  assert.equal(users.length, 2);
+  assert.equal(users.some(u => u.name === 'Andrew'), false);
 });
